a0c58c230f19db3e0f823d86f1799db1ab492abd
marius <mariusvalentin9918@gmail.com>
2025-11-01 20:32:53 +0200

    New assets added

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool IsPaused { get; set; }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
